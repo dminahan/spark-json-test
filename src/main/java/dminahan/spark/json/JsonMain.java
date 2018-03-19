ddf266188ac93bb52dea76a2a2c7321db670e0a7
dminahan <dfgriffith@gmail.com>
2018-03-19 15:24:24 +0000

    Update JsonMain.java

--- src/main/java/dminahan/spark/json/JsonMain.java
+++ src/main/java/dminahan/spark/json/JsonMain.java
@@ -35,8 +35,22 @@ public void init() {
    context=new JavaSparkContext(conf);
    sqlContext=new SQLContext(context);
 }
+   
+   /** example base starting from:  https://docs.databricks.com/spark/latest/data-sources/read-json.html
+   example.json:
+   {"string":"string1","int":1,"array":[1,2,3],"dict": {"key": "value1"}}
+{"string":"string2","int":2,"array":[2,4,6],"dict": {"key": "value2"}}
+{"string":"string3","int":3,"array":[3,6,9],"dict": {"key": "value3", "extra_key": "extra_value3"}}
+   */
 
 public void run(SparkSession sparkSession) {
+   Dataset<Row> df = spark.read.json("example.json");
+   df.printSchema();
+   df.show(false);
+   /*  Multi-line example in scala
+   val mdf = spark.read.option("multiline", "true").json("multi.json")
+   mdf.show(false)
+   */
 
 }
  
