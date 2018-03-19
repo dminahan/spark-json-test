a02bbec14850b4a8823d5ae63b7f152ffeb38322
dminahan <dfgriffith@gmail.com>
2018-03-19 17:25:49 +0000

    Update FakeJsonMain.java

--- src/main/java/dminahan/spark/json/FakeJsonMain.java
+++ src/main/java/dminahan/spark/json/FakeJsonMain.java
@@ -37,9 +37,40 @@ public void init() {
 }
    
 public void run(SparkSession sparkSession) {
-   Dataset<Row> df = sparkSession.read().json("fakeJson.json");
-   df.printSchema();
-   df.show(false);
+   Dataset<Row> records = sparkSession.read().json("fakeJson.json");
+   records.printSchema();
+   records.show(false);
+   
+   records.createOrReplaceTempView("fake_records");
+   
+   //TODO: When best to take the substring of the source "foo:" and test if the UUID is not null/empty?
+   Dataset<Row> members=records.filter(col("state").isNotNull().and(col("updated").isNotNull())
+                               .and(col("user").isNotNull()).and(col("originator").isNotNull())
+                               .and(col("originator").startsWith("foo"))
+   )
+   .select(
+      col("state"),
+      col("updated"),
+      col("user"),
+      col("originator)
+   )
+   .toDF("state","updated","user","originator");
+      
+   members.createOrReplaceView("fake");
+      
+      //Execute a sample SQL (note that the results could be written to HDFS if desired)
+      Dataset<Row> sampleSqlResults=sparkSession.sql(
+         "select state" +
+         ", updated at updatedTime" +
+         ", user" +
+         ", originator as source" +
+         ", originator as uuid" +
+         " from fake " +
+         " where originator is not null"
+      );
+      
+      sampleSqlResults.printSchema();
+      sampleSqlResults.show(10,false);
    /*  Multi-line example in scala
    val mdf = spark.read.option("multiline", "true").json("multi.json")
    mdf.show(false)
